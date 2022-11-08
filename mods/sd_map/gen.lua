4a5b987c2154942d2319ceb582291341512d9a9e
regulus79 <117475203+regulus79@users.noreply.github.com>
2022-11-07 22:05:13 -0500

    Added caves and tunnels

--- mods/sd_map/gen.lua
+++ mods/sd_map/gen.lua
@@ -60,6 +60,43 @@ function minetest.get_spawn_level(x, z)
 	return math_max(top_layer.y_top, math_floor(top_layer.y_top + top_layer.noise:get_2d({ x = x, y = z }))) + 0.5
 end
 
+-- One cave per "chunk" (which has nothing to do with mapblock size)
+local chunk_size = 40
+local cave_dimensions = vector.new(10, 3, 10)
+local get_minp_from_pos = function(pos)
+	return vector.floor(pos / chunk_size) * chunk_size
+end
+
+local vector_componentwise_divide = function(v1, v2)
+	return vector.new(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z)
+end
+
+local get_nearest_cave = function(pos)
+	local minp = get_minp_from_pos(pos)
+	--Poor Man's Hashing Function
+	math.randomseed(minp.x + minp.y + minp.z)
+	local cave_pos = vector.new(
+		math.random(minp.x + cave_dimensions.x, minp.x + chunk_size - cave_dimensions.x),
+		math.random(minp.y + cave_dimensions.y, minp.y + chunk_size - cave_dimensions.y),
+		math.random(minp.z + cave_dimensions.z, minp.z + chunk_size - cave_dimensions.z)
+	)
+	return cave_pos
+end
+
+local get_distance_to_nearest_cave = function(pos)
+	return vector.length(vector_componentwise_divide(get_nearest_cave(pos) - pos, vector.normalize(cave_dimensions)), 0)
+end
+
+local tunnel_radius = 2
+local get_distance_from_tunnel = function(pos, tunneldir, tunnelstart)
+	local offset = pos - tunnelstart
+	if vector.dot(tunneldir, offset) < 0 then
+		return tunnel_radius + 1000 --if you are behind the start of the tunnel, don't add a tunnel
+	else
+		return vector.dot(vector.normalize(vector.cross(vector.cross(tunneldir, offset), tunneldir)), offset)
+	end
+end
+
 minetest.register_on_generated(function(minp, maxp)
 	local y_top, y_bottom = maxp.y, minp.y
 
@@ -115,12 +152,45 @@ minetest.register_on_generated(function(minp, maxp)
 					-- Randomize transitions between layers using perlin noise
 					top = math_min(y_top, math_floor(layer.y_top + layer.noise:get_2d(xz_point)))
 				end
-				for _ = bottom, top do
+				for y = bottom, top do
 					-- NOTE: `math.random` is used because it is by far the fastest RNG;
 					-- determinism is not needed when randomizing nodes
 					data[y_index] = cids[math_random(1, #cids)]
+
+					local pos = vector.new(x, y, z)
+					--Create caves
+					if get_distance_to_nearest_cave(pos) < vector.length(cave_dimensions) then
+						data[y_index] = minetest.CONTENT_AIR
+					end
+
+					--Create tunnels between a couple nearby caves
+					local current_cave = get_nearest_cave(pos)
+					--Add/subtract a chunk size to find another cave.
+					local nearby_cave1 = get_nearest_cave(vector.add(pos, vector.new(chunk_size, 0, chunk_size)))
+					local nearby_cave2 = get_nearest_cave(vector.subtract(pos, vector.new(chunk_size, 0, chunk_size)))
+					local nearby_cave3 = get_nearest_cave(vector.add(pos, vector.new(chunk_size, 0, -chunk_size)))
+					local nearby_cave4 = get_nearest_cave(vector.subtract(pos, vector.new(chunk_size, 0, -chunk_size)))
+
+					local dist_tunnel1 = get_distance_from_tunnel(pos, nearby_cave1 - current_cave, current_cave)
+					if dist_tunnel1 < tunnel_radius then
+						data[y_index] = minetest.CONTENT_AIR
+					end
+					local dist_tunnel2 = get_distance_from_tunnel(pos, nearby_cave2 - current_cave, current_cave)
+					if dist_tunnel2 < tunnel_radius then
+						data[y_index] = minetest.CONTENT_AIR
+					end
+					local dist_tunnel3 = get_distance_from_tunnel(pos, nearby_cave3 - current_cave, current_cave)
+					if dist_tunnel3 < tunnel_radius then
+						data[y_index] = minetest.CONTENT_AIR
+					end
+					local dist_tunnel4 = get_distance_from_tunnel(pos, nearby_cave4 - current_cave, current_cave)
+					if dist_tunnel4 < tunnel_radius then
+						data[y_index] = minetest.CONTENT_AIR
+					end
+
 					y_index = y_index + ystride -- y++
 				end
+
 				bottom = top + 1
 			end
 			x_index = x_index + 1
