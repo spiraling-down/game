83e394f66bf3c7af0c77ecadfb9c54222743cb44
Lars Mueller <appgurulars@gmx.de>
2022-11-12 14:35:01 +0100

    Implement lamps

--- mods/sd_tools/lamp.lua
+++ mods/sd_tools/lamp.lua
@@ -1,12 +1,62 @@
 local modname = minetest.get_current_modname()
 
-local lamp_nodename = modname .. ":lamp"
+local on_duration = 5 * 60 -- 5 minutes
 
-minetest.register_node(lamp_nodename, {
-	description = "Lamp",
-	tiles = { modname .. "_light_lamp.png" },
-	walkable = true,
+local function register(variant, def)
+	local nodename = ("%s:lamp_%s"):format(modname, variant)
+	-- NOTE: Currently unused, but may be useful if made pointable/walkable
+	local box = {
+		type = "wallmounted",
+		wall_bottom = { -0.25, -0.5, -0.25, 0.25, 0.5, 0.25 },
+	}
+	minetest.register_node(
+		nodename,
+		modlib.table.complete(def, {
+			description = ("Lamp (%s)"):format(variant),
+			tiles = { ("%s_light_lamp_%s.png"):format(modname, variant) },
+			drawtype = "plantlike",
+			paramtype = "light",
+			sunlight_propagates = true,
+			-- TODO what happens when they lose the node they are mounted to?
+			paramtype2 = "wallmounted",
+			pointable = false,
+			selection_box = box,
+			walkable = false,
+			collision_box = box,
+		})
+	)
+	return nodename
+end
+
+local nodename_off = register("off", {})
+
+local nodename_on = register("on", {
 	light_source = 14,
+	on_timer = function(pos) -- turn off
+		local node = minetest.get_node(pos)
+		node.name = nodename_off
+		minetest.set_node(pos, node)
+	end,
+	after_place_node = function(pos)
+		local meta = minetest.get_meta(pos)
+		meta:set_int("gametime", minetest.get_gametime())
+		minetest.get_node_timer(pos):start(on_duration)
+	end,
 })
 
-return lamp_nodename
+minetest.register_lbm({
+	label = "Switch off lamps",
+	name = ("%s:switch_off_lamps"):format(modname),
+	nodenames = { nodename_on },
+	run_at_every_load = true,
+	action = function(pos)
+		local gametime = minetest.get_meta(pos):get_int("gametime")
+		local time_left = math.max(0, on_duration - (minetest.get_gametime() - gametime))
+		minetest.get_node_timer(pos):start(time_left)
+	end,
+})
+
+return {
+	on = nodename_on,
+	off = nodename_off,
+}
--- mods/sd_tools/light.lua
+++ mods/sd_tools/light.lua
@@ -1,6 +1,6 @@
 local require = modlib.mod.require
 local register = require("register")
-local lamp_nodename = require("lamp")
+local lamp = require("lamp")
 
 -- TODO deduplicate with manipulator
 local max_uses = 50
@@ -10,12 +10,18 @@ local wear_per_use = math.floor(max_wear / max_uses)
 return register("light", {
 	_recharge_time = 60,
 	description = "Light",
-	on_place = function(itemstack, placer, pointed_thing)
+	on_place = function(itemstack, _, pointed_thing)
 		if itemstack:get_wear() + wear_per_use > max_wear then
 			return
 		end
-		itemstack:add_wear(wear_per_use)
-		minetest.item_place(ItemStack(lamp_nodename), placer, pointed_thing)
+		local current_node = minetest.get_node(pointed_thing.above).name
+		if current_node == "air" or current_node == lamp.off then
+			itemstack:add_wear(wear_per_use)
+			minetest.set_node(pointed_thing.above, {
+				name = lamp.on,
+				param2 = minetest.dir_to_wallmounted(pointed_thing.under - pointed_thing.above),
+			})
+		end
 		return itemstack
 	end,
 })
