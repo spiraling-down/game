91b348e2f89e53becffeb43f0887f10a81eb94d9
Lars Mueller <appgurulars@gmx.de>
2022-11-13 15:12:37 +0100

    Tweak tool recharging

--- mods/sd_tools/light.lua
+++ mods/sd_tools/light.lua
@@ -3,12 +3,12 @@ local register = require("register")
 local lamp = require("lamp")
 
 -- TODO deduplicate with manipulator
-local max_uses = 50
+local max_uses = 2
 local max_wear = 65535
 local wear_per_use = math.floor(max_wear / max_uses)
 
 return register("light", {
-	_recharge_time = 60,
+	_recharge_time = 10,
 	description = "Light",
 	on_place = function(itemstack, player, pointed_thing)
 		if itemstack:get_wear() + wear_per_use > max_wear then
--- mods/sd_tools/manipulator.lua
+++ mods/sd_tools/manipulator.lua
@@ -2,12 +2,12 @@ local require = modlib.mod.require
 local register = require("register")
 local scaffolding_nodename = require("scaffolding")
 
-local max_uses = 50
+local max_uses = 4
 local max_wear = 65535
 local wear_per_use = math.floor(max_wear / max_uses)
 
 return register("manipulator", {
-	_recharge_time = 60,
+	_recharge_time = 30,
 	description = "Manipulator",
 	on_place = function(itemstack, player, pointed_thing)
 		if itemstack:get_wear() + wear_per_use > max_wear then
